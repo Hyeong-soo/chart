3aaa81d071e4a0ca3cd36daac554568c90edbf63
hyeongsoo <87709954+Hyeong-soo@users.noreply.github.com>
2022-12-11 23:37:01 +0900

    Update prices.py

--- prices.py
+++ prices.py
@@ -1,11 +1,11 @@
 import numpy as np
 turn = 0
 prices = np.zeros(shape=(1,5),dtype=np.intc)
-prices[0][0] = 1000000
+prices[0][0] = 800000
 prices[0][1] = 1000000
 prices[0][2] = 1000000
-prices[0][3] = 1000000
-prices[0][4] = 1000000
+prices[0][3] = 800000
+prices[0][4] = 500000
 
 change = np.zeros(5, dtype=np.intc)
 
