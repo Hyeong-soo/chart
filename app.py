044a01a653febd7a6008e23a521a333e7be4bb15
hyeongsoo <87709954+Hyeong-soo@users.noreply.github.com>
2022-12-11 23:53:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import prices as pr
 from time import sleep
 
 
-def view_price(a: int):
+def view_price(a):
 	if  a/1000000 > 0:
-		return str(a/1000000) + str(,)+ str((a%1000000)/1000) + str(,) + str(a%1000)
+		return str(int(a) / 1000000) + str(,)+ str((a%1000000)/1000) + str(,) + str(a%1000)
 	elif a/1000>0:
 		return str(a/1000) + str(,) + str(a%1000)
 	else return str(a)
