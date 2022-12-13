0d955a18fea57ee945930ce64e198db9ab988134
hyeongsoo <87709954+Hyeong-soo@users.noreply.github.com>
2022-12-13 11:57:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -34,10 +34,10 @@ if st.button("Reset"):
     pr.turn = 0
 cols = st.columns((1,1,2))
 cols[0].metric("SAM SMITH", format((int(pr.prices[pr.turn][0]/10000)*10000), ','), pr.ss)
-cols[0].metric("GG CHEMICAL", format((int(pr.prices[pr.turn][0]/10000)*10000), ','), pr.gc)
-cols[0].metric("MUSCAR", format((int(pr.prices[pr.turn][0]/10000)*10000), ','), pr.mc)
-cols[1].metric("KOKOa", format((int(pr.prices[pr.turn][0]/10000)*10000), ','), pr.kk)
-cols[1].metric("DTD", format((int(pr.prices[pr.turn][0]/10000)*10000), ','), pr.dt)
+cols[0].metric("GG CHEMICAL", format((int(pr.prices[pr.turn][1]/10000)*10000), ','), pr.gc)
+cols[0].metric("MUSCAR", format((int(pr.prices[pr.turn][2]/10000)*10000), ','), pr.mc)
+cols[1].metric("KOKOa", format((int(pr.prices[pr.turn][3]/10000)*10000), ','), pr.kk)
+cols[1].metric("DTD", format((int(pr.prices[pr.turn][4]/10000)*10000), ','), pr.dt)
 chart_data = pd.DataFrame(
     pr.prices,
     columns=['SAM SMITH','GG CHEMICAL','MUSCAR', 'KOKOa', 'DTD']
