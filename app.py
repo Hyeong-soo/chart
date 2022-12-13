cff41b322fbc6beedfc261cef653d75b03315c75
hyeongsoo <87709954+Hyeong-soo@users.noreply.github.com>
2022-12-13 11:55:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -33,11 +33,11 @@ if st.button("Reset"):
     pr.prices[0][4] = 500000
     pr.turn = 0
 cols = st.columns((1,1,2))
-cols[0].metric("SAM SMITH", format(pr.prices[pr.turn][0], ','), pr.ss)
-cols[0].metric("GG CHEMICAL", format(pr.prices[pr.turn][1], ','), pr.gc)
-cols[0].metric("MUSCAR", format(pr.prices[pr.turn][2], ','), pr.mc)
-cols[1].metric("KOKOa", format(pr.prices[pr.turn][0], ','), pr.kk)
-cols[1].metric("DTD", format(pr.prices[pr.turn][0], ','), pr.dt)
+cols[0].metric("SAM SMITH", format((int(pr.prices[pr.turn][0]/10000)*10000), ','), pr.ss)
+cols[0].metric("GG CHEMICAL", format((int(pr.prices[pr.turn][0]/10000)*10000), ','), pr.gc)
+cols[0].metric("MUSCAR", format((int(pr.prices[pr.turn][0]/10000)*10000), ','), pr.mc)
+cols[1].metric("KOKOa", format((int(pr.prices[pr.turn][0]/10000)*10000), ','), pr.kk)
+cols[1].metric("DTD", format((int(pr.prices[pr.turn][0]/10000)*10000), ','), pr.dt)
 chart_data = pd.DataFrame(
     pr.prices,
     columns=['SAM SMITH','GG CHEMICAL','MUSCAR', 'KOKOa', 'DTD']
